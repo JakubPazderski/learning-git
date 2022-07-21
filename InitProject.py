1d63dc64990d76ab4bb373384c3f923ce3f91ce7
Jakub Pazderski <jak.pazderski@gmail.com>
2022-07-21 19:05:09 +0200

    Add InitProject.py

---
+++ InitProject.py
@@ -0,0 +1,6 @@
+shopping_list=["chleb","bułki","pączki","marchew","ogórek","rukola"]
+shopping_dict={
+    "piekarnia":[shopping_list[0].capitalize(),shopping_list[1].capitalize(),shopping_list[2].capitalize()],
+    "warzywniak":[shopping_list[3].capitalize(), shopping_list[4].capitalize(), shopping_list[5].capitalize()]
+}
+
