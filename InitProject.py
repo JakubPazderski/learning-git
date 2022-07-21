63e552c1265c787ee2ba701f4f0dc9227751ed5f
Jakub Pazderski <jak.pazderski@gmail.com>
2022-07-21 19:07:43 +0200

    Add InitProject.py dict loop

--- InitProject.py
+++ InitProject.py
@@ -3,4 +3,6 @@ shopping_dict={
     "piekarnia":[shopping_list[0].capitalize(),shopping_list[1].capitalize(),shopping_list[2].capitalize()],
     "warzywniak":[shopping_list[3].capitalize(), shopping_list[4].capitalize(), shopping_list[5].capitalize()]
 }
-
+for shop,products in shopping_dict.items():
+    print (f"Wchodzę do {shop} i kupuję {products}.")
+    
